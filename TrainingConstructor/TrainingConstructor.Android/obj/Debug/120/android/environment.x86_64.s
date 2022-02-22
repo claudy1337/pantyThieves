0faabad730678ce76298194b361c88bfd33fa58a
darkroot <45602451+claudy1337@users.noreply.github.com>
2022-02-23 02:06:52 +0300

    project layout setting

--- TrainingConstructor/TrainingConstructor.Android/obj/Debug/120/android/environment.x86_64.s
+++ TrainingConstructor/TrainingConstructor.Android/obj/Debug/120/android/environment.x86_64.s
@@ -84,7 +84,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"fc87ee1d-730c-4432-97d8-e4cf09a35084"
+	.asciz	"f2a254d2-52ae-4fea-b5d5-8c2542262227"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
